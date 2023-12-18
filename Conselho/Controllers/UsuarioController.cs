d249eb81de1c5b2983e40a0d0ad0838cc5fe7203
Vinicius Benicio <vinicius.benicio97@gmail.com>
2023-12-17 22:15:45 -0300

    Finalizando Metodo Create,Update,Read, Delete Usuarios

--- Conselho/Controllers/UsuarioController.cs
+++ Conselho/Controllers/UsuarioController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Mvc;
 namespace Conselho.API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
     public class UsuarioController : ControllerBase
     {
         private readonly IRepository<Usuario> _usuarioRepository;
@@ -24,5 +23,44 @@ namespace Conselho.API.Controllers
             return Ok(usuarios);
         }
 
+        [HttpGet("v1/usuarios/{Id}")]
+        public IActionResult GetUsuariosById(int Id)
+        {
+            var usuarios = _usuarioRepository.GetById(Id);
+
+            return Ok(usuarios);
+        }
+
+        [HttpPost("v1/usuarios")]
+        public IActionResult PostUsuario([FromBody] Usuario usuarios)
+        {
+           _usuarioRepository.Add(usuarios);
+
+            return Ok();
+        }
+
+        [HttpPut("v1/usuarios/{Id}")]
+        public IActionResult PutUsuario(int Id)
+        {
+            var user = _usuarioRepository.GetById(Id);
+
+            if (user == null)
+                return NotFound();
+
+
+            _usuarioRepository.Update(user);
+
+            return Ok();
+        }
+
+        [HttpDelete("v1/usuarios/{Id}")]
+        public IActionResult DeleteUsuario(int Id)
+        {
+            var user = _usuarioRepository.GetById(Id);
+
+            _usuarioRepository.Delete(user);
+
+            return Ok();
+        }
     }
 }
